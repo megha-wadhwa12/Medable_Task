5032ad9e72aa193d412ee11ecaa93e9c1dc09d30
Megha Wadhwa <meghawadhwa20@gmail.com>
2025-12-16 16:08:00 +0530

    added index.html to netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,33 @@
 const serverless = require("serverless-http");
-const app = require("../../server");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("../../routes/auth");
+const userRoutes = require("../../routes/users");
+const secretStatsRoutes = require("../../routes/secret-stats");
+const adminRoutes = require("../../routes/admin");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// puzzle header
+app.use((req, res, next) => {
+  res.set("X-Secret-Challenge", "find_me_if_you_can_2024");
+  next();
+});
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users/secret-stats", secretStatsRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+
+// health
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
 
 module.exports.handler = serverless(app);
